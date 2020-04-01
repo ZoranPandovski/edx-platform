71b43b772d0b00a2ec83e2bdf06b3a7ef2a3073a
Zoran Pandovski <zoran.pandovski@gmail.com>
2020-04-01 16:42:06 +0200

    Update urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -175,6 +175,7 @@ urlpatterns = [
     url(r'^api/val/v0/', include('edxval.urls')),
     url(r'^api/tasks/v0/', include('user_tasks.urls')),
     url(r'^accessibility$', contentstore.views.accessibility, name='accessibility'),
+    url(r'^api/jupyter_graded/', include('xblock_jupyter_graded.rest.urls')
 ]
 
 if not settings.DISABLE_DEPRECATED_SIGNIN_URL:
