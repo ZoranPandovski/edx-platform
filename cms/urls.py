8a186f25e8e7c383780270623f0fe1cbdfafc92b
Zoran Pandovski <zoran.pandovski@gmail.com>
2020-04-01 17:03:04 +0200

    Update urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -173,6 +173,8 @@ urlpatterns = [
     url(r'^api/val/v0/', include('edxval.urls')),
     url(r'^api/tasks/v0/', include('user_tasks.urls')),
     url(r'^accessibility$', contentstore.views.accessibility, name='accessibility'),
+    url(r'^api/jupyter_graded/', include('xblock_jupyter_graded.rest.urls')),
+
 ]
 
 JS_INFO_DICT = {
